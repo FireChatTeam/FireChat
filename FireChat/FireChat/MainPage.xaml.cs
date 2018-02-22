21c1215e4912afdcb34c5d5485f3d46f7be2de73
Miguel <empimcf@gmail.com>
2018-02-22 23:25:26 +0100

    Sala de chat terminada

--- FireChat/FireChat/MainPage.xaml.cs
+++ FireChat/FireChat/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FireChat.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,7 @@ namespace FireChat
 
         void OnButtonClicked(object sender, EventArgs e) { 
         
-            Navigation.PushModalAsync(new Preferencias());
+            Navigation.PushModalAsync(new Chat());
         }
     }
 }
---
+++ FireChat/FireChat/View/Chat.xaml.cs
@@ -0,0 +1,37 @@
+﻿using FireChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FireChat.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Chat : ContentPage
+	{
+        ObservableCollection<Mensaje> mensajeList = new ObservableCollection<Mensaje>();
+        public Chat ()
+		{
+			InitializeComponent ();
+
+            _lstChat.ItemsSource = mensajeList;
+
+            Mensaje m1 = new Mensaje();
+            Mensaje m2 = new Mensaje();
+
+            m1.UserName = "Miguel Puertas";
+            m1.UserMessage = "No me gusta Xamarin gente :(";
+
+            m2.UserName = "Miguel Paramos";
+            m2.UserMessage = "Poooos te aguantas!";
+
+            mensajeList.Add(m1);
+            mensajeList.Add(m2);
+        }
+	}
+}
