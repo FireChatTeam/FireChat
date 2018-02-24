0ad8326ed10ceaefd413bd794bb71396186dd40c
Plaroser <https://plaroser@bitbucket.org/plaroser/pruebaandroid.git>
2018-02-24 12:18:27 +0100

    Metodos OnChanged añadidos a pantalla preferencias

--- FireChat/FireChat/View/Preferencias.xaml.cs
+++ FireChat/FireChat/View/Preferencias.xaml.cs
@@ -12,9 +12,44 @@ namespace FireChat
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Preferencias : ContentPage
 	{
-		public Preferencias ()
+        public static readonly string 
+            VIBRACION = "vibracion", 
+            SONIDO = "sonido", 
+            NOTIFICACIONES_EMERGENTES = "notificacionesEmergentes", 
+            NOTIFICACINOES_PANTALLA_INICIO = "notificacionesPantInicio";
+        private SwitchCell switchVibracion, switchSonido, switchNotificacionesEmergentes, switchNotificacionesPantInicio;
+
+
+        public Preferencias ()
 		{
 			InitializeComponent ();
+            switchVibracion = XswitchVibracion;
+            switchSonido = XswitchSonido;
+            switchNotificacionesEmergentes = XswitchNotEmergentes;
+            switchNotificacionesPantInicio = XswitchNotPantaInicio;
+            Inicializar();
 		}
-	}
+
+        private void Inicializar()
+        {
+
+        }
+
+        private void switchVibracionChanged(object sender, ToggledEventArgs e)
+        {
+            
+        }
+        private void switchSonidoChanged(object sender, ToggledEventArgs e)
+        {
+
+        }
+        private void switchNotificacionesEmergentesChanged(object sender, ToggledEventArgs e)
+        {
+
+        }
+        private void switchNotificacionesPantInicioChanged(object sender, ToggledEventArgs e)
+        {
+
+        }
+    }
 }
