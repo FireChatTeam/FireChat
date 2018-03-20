e0e64bd3cece32b9e43e625f178b601ceba04909
alejandro <alejandro.rio@outlook.es>
2018-03-20 21:59:22 +0100

    Registro funcionando

--- FireChat/FireChat/RegistrationPage.xaml.cs
+++ FireChat/FireChat/RegistrationPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FireChat.Database;
+using FireChat.Models;
+using Plugin.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,53 @@ namespace FireChat
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RegistrationPage : ContentPage
 	{
-		public RegistrationPage ()
+        private dbFirebase db = dbFirebase.getInstance();
+
+        public RegistrationPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+        }
+
+        void OnRegisterTouched(object sender, EventArgs e)
+        {
+            User userToRegist = new User(txtName.Text, txtPassword1.Text, txtMail.Text);
+
+            Task.Run(() =>
+            {
+                db.saveUser(userToRegist);
+                Notification notification = new Notification
+                    {
+                        Title = "Registro correcto",
+                        Message = "Registrado como: " + userToRegist.Name + 
+                        " con password: " + userToRegist.Password + 
+                        " y email: " + userToRegist.Email, 
+                        Vibrate = Preferencias.getVibracion()
+                    };
+                CrossNotifications.Current.Send(notification);
+            });
+        }
+
+        void OnNameTouched(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+        }
+
+        void OnMailTouched(object sender, EventArgs e)
+        {
+            txtMail.Text = "";
+        }
+
+
+        void OnPasswordTouched1(object sender, EventArgs e)
+        {
+            txtPassword1.Text = "";
+        }
+
+
+        void OnPasswordTouched2(object sender, EventArgs e)
+        {
+            txtPassword2.Text = "";
+        }
+
+    }
 }
