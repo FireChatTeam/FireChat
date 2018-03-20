95b6899c8a855e1ec8dad39db997fb57c2ab57b4
alejandro <alejandro.rio@outlook.es>
2018-03-20 21:49:17 +0100

    Login funcional

--- FireChat/FireChat/LoginPage.xaml.cs
+++ FireChat/FireChat/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FireChat.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +7,64 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using FireChat.Models;
+using FireChat.View;
+using Plugin.Notifications;
 
 namespace FireChat
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
 	{
-		public LoginPage ()
+        private dbFirebase db = dbFirebase.getInstance();
+
+        public LoginPage ()
 		{
 			InitializeComponent ();
           
 		}
-	}
+
+        void OnLoginTouched(object sender, EventArgs e)
+        {
+            User user = new User(txtName.Text, txtPassword.Text, "correo@correo.es");
+            bool loginOK = false;
+
+            Task.Run(() =>
+            {
+                loginOK = db.checkLogin(user).Result;
+                Notification notification;
+                if (loginOK)
+                {
+                    //Navigation.PushModalAsync(new ListarChats()); no funciona
+                    notification = new Notification
+                    {
+                        Title = "Login correcto",
+                        Message = "Logeado como: " + user.Name,
+                        Vibrate = Preferencias.getVibracion()
+                    };
+
+                } else
+                {
+                    notification = new Notification
+                    {
+                        Title = "Login incorrecto",
+                        Message = "Nombre o contraseña erróneos",
+                        Vibrate = Preferencias.getVibracion()
+                    };
+                }
+                CrossNotifications.Current.Send(notification);
+            });
+            
+        }
+
+        void OnNameTouched(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+        }
+
+        void OnPasswordTouched(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+        }
+    }
 }
