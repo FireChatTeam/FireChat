40dae3f72cb94bad21fdc30df22ff2cb2e818de3
alejandro <alejandro.rio@outlook.es>
2018-03-20 21:31:08 +0100

    modelo User creado

---
+++ FireChat/FireChat/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireChat.Models
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+
+        public User(string name, string password, string email)
+        {
+            this.Name = name;
+            this.Password = password;
+            this.Email = email;
+        }
+
+
+        public static User getDefaultUser()
+        {
+            return new User("cenec", "admincenec", "cenec@mail.es");
+        }
+    }
+}
